19da0aead244106ab53d3b4bc8af412b02554460
Chris Sach <christophersach90@gmail.com>
2023-05-25 14:46:47 +0100

    fixed some styling

--- src/assets/icons/cohortSoftwareDevelopmentIcon.js
+++ src/assets/icons/cohortSoftwareDevelopmentIcon.js
@@ -14,43 +14,6 @@ const CohortSoftwareDevelopmentIcon = () => {
           fill="white"
         />
       </g>
-      <defs>
-        <filter
-          id="filter0_d_193_7913"
-          x="0"
-          y="0"
-          width="64"
-          height="64"
-          filterUnits="userSpaceOnUse"
-          color-interpolation-filters="sRGB"
-        >
-          <feFlood flood-opacity="0" result="BackgroundImageFix" />
-          <feColorMatrix
-            in="SourceAlpha"
-            type="matrix"
-            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-            result="hardAlpha"
-          />
-          <feOffset dy="4" />
-          <feGaussianBlur stdDeviation="2" />
-          <feComposite in2="hardAlpha" operator="out" />
-          <feColorMatrix
-            type="matrix"
-            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
-          />
-          <feBlend
-            mode="normal"
-            in2="BackgroundImageFix"
-            result="effect1_dropShadow_193_7913"
-          />
-          <feBlend
-            mode="normal"
-            in="SourceGraphic"
-            in2="effect1_dropShadow_193_7913"
-            result="shape"
-          />
-        </filter>
-      </defs>
     </svg>
   );
 };
